8798e058d5484833232f22cb873292df4d43463a
Eduard <eduard@eduards-mac.local>
2018-04-18 03:18:01 -0700

    Initial Commit

---
+++ Gamev3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gamev3
+//
+//  Created by Eduard on 18/04/2018.
+//  Copyright © 2018 Eduard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
